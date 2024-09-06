0ec3d073a450c185a1bb756dc97bf021d3a8e23c
John Paul Tuohy <johnpaul.tuohy@gmail.com>
2024-09-06 14:52:20 +0100

    Disable SSR for md editing

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -7,7 +7,7 @@ import {
   CalloutText,
   Text,
 } from '@radix-ui/themes';
-import SimpleMDE from 'react-simplemde-editor';
+import dynamic from 'next/dynamic';
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
@@ -19,6 +19,10 @@ import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
 import { on } from 'events';
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
