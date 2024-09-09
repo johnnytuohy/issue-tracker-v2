4bc8bf42e1a2735da2afe3bf098451ad2c6a00f0
John Paul Tuohy <johnpaul.tuohy@gmail.com>
2024-09-09 13:48:50 +0100

    Remove duplicate skeletons

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { z } from 'zod';
+import { issueSchema } from '@/app/validationSchemas';
 import prisma from '@/prisma/client';
-import { createIssueSchema } from '@/app/validationSchemas';
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const validation = createIssueSchema.safeParse(body);
+  const validation = issueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
 
