dcfc7b84a908a3d26a8d2bcce13e2590b1ef385e
John Paul Tuohy <johnpaul.tuohy@gmail.com>
2024-09-09 11:55:45 +0100

    Build an API for deleting issues.

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -26,3 +26,17 @@ export async function PATCH(
   });
   return NextResponse.json(updatedIssue);
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue)
+    return NextResponse.json({ error: 'Issue not found' }, { status: 404 });
+
+  await prisma.issue.delete({ where: { id: issue.id } });
+  return NextResponse.json({});
+}
