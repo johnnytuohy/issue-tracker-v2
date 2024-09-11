9b111dd2053b2938de5413b35a4ce38bfea43f6b
John Paul Tuohy <johnpaul.tuohy@gmail.com>
2024-09-11 16:42:13 +0100

    Build the asignee select component

---
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { Select } from '@radix-ui/themes';
+import React from 'react';
+
+const AssigneeSelect = () => {
+  return (
+    <Select.Root>
+      <Select.Trigger />
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          <Select.Item value="1">John Doe</Select.Item>
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default AssigneeSelect;
