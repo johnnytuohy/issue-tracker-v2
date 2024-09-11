49a663a39ece1e2105e2247faa7e8f6a5ed803bd
John Paul Tuohy <johnpaul.tuohy@gmail.com>
2024-09-11 18:35:10 +0100

    Fetch data with React Query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,19 +1,27 @@
 'use client';
 import { User } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Skeleton } from '@/app/components';
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get<User[]>('/api/users').then(res => res.data),
+    staleTime: 1000 * 60,
+    retry: 3,
+  });
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>('/api/users');
-      setUsers(data);
-    };
-    fetchUsers();
-  }, []);
+  if (isLoading) return <Skeleton height={30} />;
+
+  if (error) return null;
+
   return (
     <Select.Root>
       <Select.Trigger />
